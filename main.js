1cc6914cd4545b5dfc2856aebced786b771903f0
Tegsingh1 <78391323+Tegsingh1@users.noreply.github.com>
2022-01-21 19:07:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+if(keypressed == '70' )
+{
+    new_image("");
+    console.log("f");
+}
+
+if(keypressed == '66' )
+{
+    new_image("");
+    console.log("b");
+}
+
+if(keypressed == '76' )
+{
+    new_image("");
+    console.log("l");
+}
+
+if(keypressed == '82' )
+{
+    new_image("");
+    console.log("r");
+}
+if(keypressed == '72' )
+{
+    new_image("");
+    console.log("h");
+}
+if(keypressed == '38' )
+{
+    up();
+    console.log("f");
+}
